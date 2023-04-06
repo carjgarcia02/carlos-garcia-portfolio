5b4b9a208948f50390e295426d02fcb0af843ac5
carjgarcia02 <carjgarcia02@gmail.com>
2023-04-05 20:46:50 -0500

    Finish presentation section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
 import Sidenav from "./components/Sidenav";
+import Presentation from "./components/Presentation";
 
 function App() {
   return (
     <div>
       <Sidenav />
+      <Presentation />
     </div>
   );
 }
